6f4db5fc791b27ea0930d01f038833cf4af78c17
Leonel Donati <leonelroman321@gmail.com>
2025-04-18 18:25:46 -0300

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react()],
+  adapter: netlify(),
 });
