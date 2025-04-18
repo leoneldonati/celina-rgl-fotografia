99a0b58bf3c8f4d28dba3a36a2c29087df6b8a02
Leonel Donati <leonelroman321@gmail.com>
2025-04-18 14:40:06 -0300

    added carousel

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import asset1 from "@assets/carousel/1.avif";
-import asset2 from "@assets/carousel/2.avif";
-import asset3 from "@assets/carousel/3.avif";
-import asset4 from "@assets/carousel/4.avif";
-export default function Carousel() {
+import LeftArrow from "@assets/svg/caret-left.svg";
+import RightArrow from "@assets/svg/caret-right.svg";
+
+interface Props {
+  assets: ImageMetadata[];
+}
+export default function Carousel({ assets }: Props) {
   const [position, setPosition] = useState(0);
-  const assets = [asset1, asset2, asset3, asset4];
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -15,27 +16,60 @@ export default function Carousel() {
     return () => clearInterval(timer);
   }, []);
   return (
-    <picture className="max-h-full overflow-x-hidden relative rounded-md">
-      <div
-        className="flex transition-transform"
-        style={{ transform: `translateX(-${position * 100}%)` }}
-      >
-        {assets.map((image, index) => (
-          <img
-            src={image.src}
-            key={index}
-            loading="lazy"
-            width={image.width}
-            height={image.height}
-          />
-        ))}
-      </div>
+    <div className="flex flex-col items-center gap-2">
+      <picture className="max-h-full overflow-x-hidden relative rounded-md">
+        <div
+          className="flex transition-transform"
+          style={{ transform: `translateX(-${position * 100}%)` }}
+        >
+          {assets.map((image, index) => (
+            <img
+              src={image.src}
+              key={index}
+              loading="lazy"
+              width={image.width}
+              height={image.height}
+              className={index === position ? "animate-zoom" : ""}
+            />
+          ))}
+        </div>
 
-      <div className="absolute bottom-2 z-10 grid place-items-center w-full">
-        {assets.map((img, index) => (
-          <span className=" w-2 h-2 rounded-full flex"></span>
-        ))}
+        <div className="absolute bottom-2 z-10 flex items-center justify-center gap-1 w-full">
+          {assets.map((_, index) => (
+            <span
+              onClick={() => setPosition(index)}
+              style={{
+                backgroundColor:
+                  index === position ? "var(--color-brand-1)" : "#aaa",
+              }}
+              className=" w-3 h-3 rounded-full flex"
+            ></span>
+          ))}
+        </div>
+      </picture>
+
+      <div className="flex items-center gap-1">
+        <button className="transition-transform active:scale-95">
+          <img
+            src={LeftArrow.src}
+            width={LeftArrow.width}
+            height={LeftArrow.height}
+            loading="lazy"
+          />
+        </button>
+
+        <strong>
+          {position + 1}/{assets.length}
+        </strong>
+        <button className="transition-transform active:scale-95">
+          <img
+            src={RightArrow.src}
+            width={RightArrow.width}
+            height={RightArrow.height}
+            loading="lazy"
+          />
+        </button>
       </div>
-    </picture>
+    </div>
   );
 }
